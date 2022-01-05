3740a9da88503772fb3bdd54d2a88f50a2fc79e5
Tomq <kosibat9846@gmail.com>
2022-01-05 13:57:38 +0100

    changed to async

--- store/modules/loadedProducts/actions.js
+++ store/modules/loadedProducts/actions.js
@@ -1,26 +1,31 @@
+import xios from "axios";
 import axios from "axios";
 
 export default{
 
-     nuxtServerInit(vuexContext, context) {
-        return axios
-          .get('http://localhost:8000/nosql-products')
-          .then(res => {
-            const productsArray = [];
-            for (const key in res.data) {
-              productsArray.push({ ...res.data[key], _id: key });
-            }
-            vuexContext.commit("setProducts", productsArray);
-          })
-          .catch(e => context.error(e));
-      },
+     // nuxtServerInit(vuexContext, context) {
+     //    return axios
+     //      .get('http://localhost:8000/nosql-products')
+     //      .then(res => {
+     //        const productsArray = [];
+     //        for (const key in res.data) {
+     //          productsArray.push({ ...res.data[key], _id: key });
+     //        }
+     //        vuexContext.commit("setProducts", productsArray);
+     //      })
+     //      .catch(e => context.error(e));
+     //  },
+     //
+     //
+     //
+     //   setProducts(vuexContext, products) {
+     //    vuexContext.commit("setProducts", products);
+     //  },
 
-
-
-       setProducts(vuexContext, products) {
-        vuexContext.commit("setProducts", products);
-      },
-
-
+  async loadedProducts({commit}) {
+    const loadedProducts = (await axios.get('http://localhost:8000/nosql-products')).data;
+    console.log(loadedProducts)
+    commit("setProducts",{loadedProducts})
+  }
 
 };
